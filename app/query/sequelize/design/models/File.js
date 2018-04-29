12c76e06e204a8c51626b2fe58c9cfd4e0674c99
jaehunpark <setyourmindpark@gmail.com>
2018-04-29 19:38:32 +0900

    수정

--- app/query/sequelize/design/models/File.js
+++ app/query/sequelize/design/models/File.js
@@ -9,6 +9,7 @@ module.exports = {
             subDirName: { type: Sequelize.STRING(50), allowNull: false },
             viewFileName: { type: Sequelize.STRING(200), allowNull: false },
             phisicalFileName: { type: Sequelize.STRING(200), allowNull: false },
+            url: { type: Sequelize.STRING(400), allowNull: true },
             useYn: { type: Sequelize.CHAR(1), allowNull: false, defaultValue: 'y' },
             createdAt: { type: Sequelize.DATE, allowNull: false, defaultValue: Sequelize.fn('NOW') },
             updatedAt: { type: Sequelize.DATE, allowNull: false, defaultValue: Sequelize.fn('NOW') }
